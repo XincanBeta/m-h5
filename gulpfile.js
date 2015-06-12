7cd09a86863ee08cbf787fe0f7712c62a79b18b4
lichao <wenling.lichao@gmail.com>
2015-06-12 19:40:18 +0800

    gulp ruby sass 配置

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('styles', function () {
-  return sass('client/assets/styles')
+gulp.task('sass-client', function () {
+  return sass('client/assets/styles', {container: 'gulp-ruby-sass-client'})
     .on('error', function (err) {
       console.error('Error!', err.message);
     })
@@ -14,11 +14,32 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('client/assets/styles'));
 });
 
-gulp.task('default', function () {
-  gulp.start('styles')
+gulp.task('sass-test', function () {
+  return sass('test/animation', {container: 'gulp-ruby-sass-test'})
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+    })
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('test/animation'));
 });
 
+gulp.task('default', function () {
+  gulp.start('sass')
+});
+
+gulp.task('sass', ['sass-client', 'sass-test'])
+
 // 监听
-gulp.watch('client/styles/main.scss', ['styles']);
+gulp.watch('client/assets/styles/main.scss', ['sass-client']);
+gulp.watch('test/animation/style.scss', ['sass-test']);
+
+
+
+
+
+
 
 
